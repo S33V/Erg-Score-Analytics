fc56b6cb063f9d7d8c43d19e73c7aba4590a7ccb
Severiano Badajoz <sevebadajoz@gmail.com>
2018-02-28 12:49:13 -0800

    Add functionality

--- src/com/sevebadajoz/ErgScoreAnalytics/model/Rower.java
+++ src/com/sevebadajoz/ErgScoreAnalytics/model/Rower.java
@@ -1,4 +1,32 @@
 package com.sevebadajoz.ErgScoreAnalytics.model;
 
 public class Rower {
+    private String name;
+
+    public Rower(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Rower rower = (Rower) o;
+
+        return name != null ? name.equals(rower.name) : rower.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
