a279d27e958510567db62dde66a696c09ff8129c
Severiano Badajoz <sevebadajoz@gmail.com>
2018-02-28 13:26:49 -0800

    Add basic functionalityi

--- src/com/sevebadajoz/ErgScoreAnalytics/model/Split.java
+++ src/com/sevebadajoz/ErgScoreAnalytics/model/Split.java
@@ -1,4 +1,30 @@
 package com.sevebadajoz.ErgScoreAnalytics.model;
 
 public class Split {
+    private String textForm;
+    private double seconds;
+
+    public Split(String textForm) {
+        this.textForm = textForm;
+        createSeconds();
+    }
+
+    public double textToSeconds(String textForm) {
+        double min = Double.parseDouble(textForm.substring(0, 1));
+        double sec = Double.parseDouble(textForm.substring(3));
+        return 60 * min + sec;
+    }
+
+    public double textToSeconds(){
+        return textToSeconds(textForm);
+    }
+
+
+    private void createSeconds(){
+        seconds = textToSeconds();
+    }
+
+    public String toString() {
+        return textForm;
+    }
 }
