4d113b934ad1e8fc3af482ed80da7faa4f9b04cf
Severiano Badajoz <sevebadajoz@gmail.com>
2018-02-28 12:42:19 -0800

    Add database constants

--- src/com/sevebadajoz/ErgScoreAnalytics/controller/Controller.java
+++ src/com/sevebadajoz/ErgScoreAnalytics/controller/Controller.java
@@ -1,4 +1,16 @@
 package com.sevebadajoz.ErgScoreAnalytics.controller;
 
 public class Controller {
+    public static final String DB_NAME = "erg_score_analytics.db";
+
+    public static final String LINEUPS_TABLE_NAME = "lineups";
+    public static final String[] LINEUPS_FIELD_NAMES = {"id", "stroke", "7", "6", "5", "4", "3", "2", "bow"};
+    public static final String[] LINEUPS_FIELD_TYPES = {"INTEGER PRIMARY KEY", "INTEGER", "INTEGER", "INTEGER",
+            "INTEGER", "INTEGER", "INTEGER", "INTEGER", "INTEGER"};
+
+    public static final String ROWERS_TABLE_NAME = "rowers";
+    public static final String[] ROWERS_FIELD_NAMES = {"id", "name"};
+    public static final String[] ROWERS_FIELD_TYPES = {"INTEGER PRIMARY KEY", "TEXT"};
+
+
 }
