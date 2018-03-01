dd7f4f38ebe0c64c413d7b6b468111e2c727f112
Severiano Badajoz <sevebadajoz@gmail.com>
2018-03-01 15:16:21 -0800

    Create operation MainScene, handle browseButton press

--- src/com/sevebadajoz/ErgScoreAnalytics/view/MainScene.java
+++ src/com/sevebadajoz/ErgScoreAnalytics/view/MainScene.java
@@ -1,20 +1,47 @@
 package com.sevebadajoz.ErgScoreAnalytics.view;
 
+import com.sevebadajoz.ErgScoreAnalytics.model.ViewSwitch;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class MainScene extends Application {
 
+    @FXML
+    private Button browseButton;
+    @FXML
+    private TextField filePath;
+    @FXML
+
+    private File selectedFile;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        ViewSwitch.setStage(primaryStage);
+
+        browseButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open Spreadsheet");
+                selectedFile = fileChooser.showOpenDialog(primaryStage);
+                filePath.setText(selectedFile.getPath());
+            }
+        });
+
+
     }
+
 
 
     public static void main(String[] args) {
