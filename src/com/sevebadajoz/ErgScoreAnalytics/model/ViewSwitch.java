1f082000a105cb12d1118b141074f0fa203cf9de
Severiano Badajoz <sevebadajoz@gmail.com>
2018-03-01 13:43:13 -0800

    Edit scene constants

--- src/com/sevebadajoz/ErgScoreAnalytics/model/ViewSwitch.java
+++ src/com/sevebadajoz/ErgScoreAnalytics/model/ViewSwitch.java
@@ -7,17 +7,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ViewSwitch {
-    public static final String BOAT_LIST_SCENE = "com/sevebadajoz/stroketiming/view/boatList.fxml";
-    public static final String ADD_BOAT_SCENE = "com/sevebadajoz/stroketiming/view/addBoat.fxml";
-    public static final String ADD_LINEUP_SCENE = "com/sevebadajoz/stroketiming/view/addLineup.fxml";
-    public static final String ADD_ROWER_SCENE = "com/sevebadajoz/stroketiming/view/addRower.fxml";
-    public static final String ADD_COXSWAIN_SCENE = "com/sevebadajoz/stroketiming/view/addCoxswain.fxml";
-    public static final String PRACTICE_LIST_SCENE = "com/sevebadajoz/stroketiming/view/practiceList.fxml";
-    public static final String ADD_PRACTICE_SCENE = "com/sevebadajoz/stroketiming/view/addPractice.fxml";
-    public static final String VIEW_ROWERS = "com/sevebadajoz/stroketiming/view/viewRowers.fxml";
-    public static final String VIEW_COXSWAINS = "com/sevebadajoz/stroketiming/view/viewCoxswains.fxml";
-    public static final String EDIT_ROWER_SCENE = "com/sevebadajoz/stroketiming/view/editRower.fxml";
-
+    public static final String BOAT_LIST_SCENE = "com/sevebadajoz/ErgScoreAnalytics/view/boatList.fxml";
+    public static final String ADD_BOAT_SCENE = "com/sevebadajoz/ErgScoreAnalytics/view/addBoat.fxml";
+    public static final String ADD_LINEUP_SCENE = "com/sevebadajoz/ErgScoreAnalytics/view/addLineup.fxml";
+    public static final String MAIN_SCENE = "com/sevebadajoz/ErgScoreAnalytics/view/main.fxml";
     public static Stage activeStage;
     public static Stage window;
 
