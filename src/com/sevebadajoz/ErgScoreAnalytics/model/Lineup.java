8aa1e02725d84afdc803860975b568a59b351256
Severiano Badajoz <sevebadajoz@gmail.com>
2018-03-01 13:41:04 -0800

    Add array initialization

--- src/com/sevebadajoz/ErgScoreAnalytics/model/Lineup.java
+++ src/com/sevebadajoz/ErgScoreAnalytics/model/Lineup.java
@@ -1,7 +1,5 @@
 package com.sevebadajoz.ErgScoreAnalytics.model;
 
-import javax.sound.sampled.Line;
-
 public class Lineup {
     private Rower[] rowers;
     private Split[] splits;
@@ -11,6 +9,7 @@ public class Lineup {
 
     public Lineup(Rower stroke, Rower seven, Rower six, Rower five, Rower four, Rower three, Rower two, Rower bow) {
         rowers = new Rower[]{stroke, seven, six, five, four, three, two, bow};
+        splits = new Split[8];
         double seconds = 0;
         for (int i = 0; i < rowers.length; i++) {
             splits[i] = rowers[i].getSplit();
