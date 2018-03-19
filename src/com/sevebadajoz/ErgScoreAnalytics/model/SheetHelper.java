36b7344ef00d051da99093531fe4b6cad8d6817e
Severiano Badajoz <sevebadajoz@gmail.com>
2018-03-19 10:38:12 -0700

    Add getColValues method to grab row data

--- src/com/sevebadajoz/ErgScoreAnalytics/model/SheetHelper.java
+++ src/com/sevebadajoz/ErgScoreAnalytics/model/SheetHelper.java
@@ -3,10 +3,10 @@ package com.sevebadajoz.ErgScoreAnalytics.model;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -21,7 +21,8 @@ public class SheetHelper {
     public SheetHelper(FileInputStream file) throws IOException {
         this.file = file;
         workbook = new XSSFWorkbook(file);
-        sheet = workbook.getSheetAt(0);
+        //TODO: Ask user what sheet
+        sheet = workbook.getSheetAt(2);
     }
 
     public SheetHelper(FileInputStream file, int sheetNum) throws IOException {
@@ -41,12 +42,29 @@ public class SheetHelper {
     }
     public String[] getRowValues(int colNum) {
         Iterator<Row> rowIterator = sheet.rowIterator();
+        DataFormatter df = new DataFormatter();
         rowIterator.next();
         String[] ret = new String[sheet.getPhysicalNumberOfRows() - 1];
         int i = 0;
         while (rowIterator.hasNext()) {
-            ret[i] = rowIterator.next().getCell(i).getStringCellValue();
+            ret[i] = df.formatCellValue(rowIterator.next().getCell(i));
         }
         return ret;
+    }
+
+    public String[] getColValues(int rowNum) {
+        Row row = sheet.getRow(rowNum);
+        DataFormatter df = new DataFormatter();
+        String[] ret = new String[row.getPhysicalNumberOfCells()];
+        for (int i = 0; i < row.getPhysicalNumberOfCells(); i++) {
+            Cell cell = row.getCell(i);
+            FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+            ret[i] = df.formatCellValue(cell, evaluator);
+        }
+        return ret;
+    }
+
+    public XSSFSheet getSheet() {
+        return sheet;
     }
 }
