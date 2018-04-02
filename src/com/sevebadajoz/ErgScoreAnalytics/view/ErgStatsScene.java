aa02b2323ee6f7a24e558bfb64d8b60140bb7bd0
Severiano Badajoz <sevebadajoz@gmail.com>
2018-04-02 11:04:59 -0700

    Create ergStats.fxml and ErgStatsScene.java

---
+++ src/com/sevebadajoz/ErgScoreAnalytics/view/ErgStatsScene.java
@@ -0,0 +1,13 @@
+package com.sevebadajoz.ErgScoreAnalytics.view;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ErgStatsScene implements Initializable{
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
