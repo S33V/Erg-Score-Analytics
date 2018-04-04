ec882dc47a1aa3a63781a052bb37eb2621b1764a
Severiano Badajoz <sevebadajoz@gmail.com>
2018-04-03 18:16:28 -0700

    Create test class for Split

---
+++ test/com/sevebadajoz/ErgScoreAnalytics/model/SplitTest.java
@@ -0,0 +1,38 @@
+package com.sevebadajoz.ErgScoreAnalytics.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SplitTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void textToSeconds() {
+    }
+
+    @Test
+    void textToSeconds1() {
+    }
+
+    @Test
+    void secondsToString() {
+    }
+
+    @Test
+    void toString() {
+    }
+
+    @Test
+    void weightAdj() {
+    }
+
+}
