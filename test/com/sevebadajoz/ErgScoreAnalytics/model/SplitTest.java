407ec639047530a5bc72e29e90dd3a53c70876ca
Severiano Badajoz <sevebadajoz@gmail.com>
2018-04-10 13:16:24 -0700

    Generate testing cases for Split

--- test/com/sevebadajoz/ErgScoreAnalytics/model/SplitTest.java
+++ test/com/sevebadajoz/ErgScoreAnalytics/model/SplitTest.java
@@ -7,32 +7,39 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SplitTest {
+
+    Split testSplit;
+
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        testSplit = new Split("01:38.7");
     }
 
     @Test
     void textToSeconds() {
+        assertEquals(98.7, testSplit.textToSeconds());
     }
 
     @Test
     void textToSeconds1() {
+        assertEquals(60.0, testSplit.textToSeconds("01:00.0"));
+        assertEquals(90.0, testSplit.textToSeconds("01:30.0"));
+        assertEquals(95.0, testSplit.textToSeconds("01:35.0"));
+        assertEquals(95.5, testSplit.textToSeconds("01:35.5"));
     }
 
     @Test
     void secondsToString() {
+        assertEquals("01:00.0", Split.secondsToString(60.0));
+        assertEquals("01:30.0", Split.secondsToString(90.0));
+        assertEquals("01:35.0", Split.secondsToString(95.0));
+        assertEquals("01:35.5", Split.secondsToString(95.5));
     }
 
-    @Test
-    void toString() {
-    }
 
     @Test
     void weightAdj() {
+        assertEquals("01:32.0", testSplit.weightAdj(197.0).toString());
     }
 
 }
