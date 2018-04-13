5c419181ecd677e209a7acb9947c60c1ed30b29f
Severiano Badajoz <sevebadajoz@gmail.com>
2018-04-13 00:15:22 -0700

    Properly implement FileChooser, showing the file in the textview. Also add exit button

--- src/com/sevebadajoz/ErgScoreAnalytics/view/ImportSheetScene.java
+++ src/com/sevebadajoz/ErgScoreAnalytics/view/ImportSheetScene.java
@@ -7,7 +7,9 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
@@ -31,18 +33,30 @@ public class ImportSheetScene implements Initializable{
 
     private Controller controller = Controller.getInstance();
 
+    @FXML
+    public Object exitCheck() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to exit?");
+        alert.showAndWait()
+                .filter(response -> response == ButtonType.OK)
+                .ifPresent(response -> System.exit(0));
+        return this;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         browseButton.setOnAction(actionEvent ->  {
                 FileChooser fileChooser = new FileChooser();
+                fileChooser.setInitialDirectory(new File(System.getProperty("user.home"), "Downloads"));
                 fileChooser.setTitle("Open Spreadsheet");
 
                 selectedFile = fileChooser.showOpenDialog(((Node)actionEvent.getSource()).getScene().getWindow());
+                if(selectedFile != null)
+                    filePath.setText(selectedFile.getPath());
         });
 
         submitButton.setOnAction(event -> {
             try {
-                if(controller.openSheet(new FileInputStream(selectedFile)))
+                if(controller.openSheet(new FileInputStream(filePath.getText())))
                 ViewSwitch.loadScene("Select Lineup", ViewSwitch.BOAT_LIST_SCENE);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
