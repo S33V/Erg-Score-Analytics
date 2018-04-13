a901efb65150b71aeb6214ec7a2242c258c39d45
Severiano Badajoz <sevebadajoz@gmail.com>
2018-04-12 23:50:10 -0700

    Remove toolbar from window

--- src/com/sevebadajoz/ErgScoreAnalytics/view/MainScene.java
+++ src/com/sevebadajoz/ErgScoreAnalytics/view/MainScene.java
@@ -2,18 +2,12 @@ package com.sevebadajoz.ErgScoreAnalytics.view;
 
 import com.sevebadajoz.ErgScoreAnalytics.model.ViewSwitch;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import javax.swing.text.View;
 
 public class MainScene extends Application {
-
-    private double xOffset = 0.0;
-    private double yOffset = 0.0;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
