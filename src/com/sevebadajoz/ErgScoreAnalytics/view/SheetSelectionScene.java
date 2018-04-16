8d0f25a9a71d2ca6dd54fae24c35c082641b5507
Severiano Badajoz <sevebadajoz@gmail.com>
2018-04-15 17:48:59 -0700

    Create sheetSelection.fxml and SheetSelectionScene.java

---
+++ src/com/sevebadajoz/ErgScoreAnalytics/view/SheetSelectionScene.java
@@ -0,0 +1,13 @@
+package com.sevebadajoz.ErgScoreAnalytics.view;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SheetSelectionScene implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
